e00562366f47af90528086a9c3e7903b6ff20530
Joel Ordoñez <o.joelf15@gmail.com>
2025-02-05 20:56:05 -0300

    adding fixes directly

--- guide-component.js
+++ guide-component.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
     const modalContainer = document.getElementById('modal-container');
-
     // Cargar el modal en la página
     modalContainer.innerHTML = `
         <div id="guideModal" class="modal">
@@ -10,11 +9,10 @@ document.addEventListener('DOMContentLoaded', function () {
             </div>
         </div>
     `;
-
     const modal = document.getElementById('guideModal');
     const guideContent = document.getElementById('guideContent');
     const closeModal = document.querySelector('.close');
-
+    // Función para cargar contenido dinámicamente en el modal
     function loadHTML(url) {
         fetch(url)
             .then(response => response.text())
@@ -27,28 +25,28 @@ document.addEventListener('DOMContentLoaded', function () {
                 console.error('Error al cargar el archivo:', error);
             });
     }
-
+    // Abre el modal con contenido específico al hacer clic en los enlaces
     document.getElementById('guia-text').addEventListener('click', function (event) {
         event.preventDefault();
         loadHTML('guia.html');
     });
-
     document.getElementById('regla-text').addEventListener('click', function (event) {
         event.preventDefault();
         loadHTML('reglas.html');
     });
-
     document.getElementById('sitios-text').addEventListener('click', function (event) {
         event.preventDefault();
         loadHTML('sitios.html');
     });
-
+    // Cerrar el modal al hacer clic en el botón de cierre
     closeModal.addEventListener('click', function () {
         modal.style.display = 'none';
     });
-
+    // Cerrar el modal si se hace clic fuera de la ventana del modal
     window.addEventListener('click', function (event) {
-        if (event.target === modal) {
+        const modal = document.getElementById('guideModal');
+        const modalContent = document.querySelector('.modal-content');
+        if (!modalContent.contains(event.target) && event.target !== modal) {
             modal.style.display = 'none';
         }
     });
